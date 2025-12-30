d9c50f815eeba175bee2ce3b711b96583a393b98
sankar <sankar24.k@email.com>
2025-12-30 18:15:15 +0530

    css changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables)
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,46 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'HR-Management';
+
+  public config: any ={
+  type:'bar', //this denotes tha type of chart
+
+data:{
+labels:['JAN','FEB','MAR'],
+
+datasets:[
+
+{ 
+  label:'UI UX',
+  data:['123','260','105','285'],
+  backgroundColor:'#38ce3c',
+},
+
+{
+  label:'Development',
+  data:['120','140','180','220'],
+  backgroundColor:'#ff4d6b',
+},
+
+
+{
+  label:'Testing',
+  data:['180','120','60','134'],
+  backgroundColor:'#ffde73',
+},
+
+
+],
+},
+options:{
+  aspectRatio: 1,
+},
+  };
+chart: any;
+ngOnInit(): void {  
+  this.chart = new Chart('MyChart', this.config);
+}
+
 }
